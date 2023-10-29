948aadacef4a8b58e241eb554ad7a53d1460df5d
Kadda Aoues <houes_k@yahoo.fr>
2023-10-29 11:14:43 +0300

    write a constructor

--- strings/CLine.h
+++ strings/CLine.h
@@ -7,10 +7,15 @@ namespace text {
 
 	class CLine {
 		std::vector<CWord> _line;
+		size_t            _max_char_inline;
 		
 		constexpr bool is_line(const std::string& str_lines) {}
+
 	public:
 
+		CLine(const std::string& line) {
+
+		}
 		
 	};
 
