08e49ae58988153e318d4aaac8216d1440d80d3a
Kadda Aoues <houes_k@yahoo.fr>
2023-10-30 11:18:48 +0300

    add comment, and function space count body

--- strings/CLine.h
+++ strings/CLine.h
@@ -5,15 +5,29 @@
 
 namespace text {
 
-	class CLine {
+
+	class CLine {   // we shoud change the designe of CLine, or we consider space as word also.
 		std::vector<CWord> _line;
-		size_t            _max_char_inline;
+		const size_t       _max_char_inline;
+		std::vector<int>   _space_count;
 		
 		constexpr bool is_line(const std::string& str_lines) {}
 
+		constexpr int space_count_in_string(const std::string& text) {
+
+		}
+
 	public:
 
-		CLine(const std::string& line) {
+		CLine(const std::string& line, const size_t max_char_in_line = 125i64)
+			: _max_char_inline{max_char_in_line}
+		{
+			// check if line is greater than 125;
+			auto _line = line;
+			if (line.size() > 125) _line = line.substr(0, 124);
+
+			auto vString = Str::TextToWords(_line);
+
 
 		}
 		
