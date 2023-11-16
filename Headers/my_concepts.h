bcb77922ec9768c5b121a6de9b9ac908193a57d3
Kadda Aoues <houes_k@yahoo.fr>
2023-11-16 14:37:32 +0300

    implement some concepts

--- Headers/my_concepts.h
+++ Headers/my_concepts.h
@@ -1,56 +1,15 @@
 #pragma once
 #include <type_traits>
 #include <concepts>
+#include <MyLib/my_metaprog.h>
 
 // some metaprogramming 
 
 namespace my_concept {
 
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// 
-	//    Meta programming concepts 
-	// 
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	// 1. ARE SAME ALL ARGUMENT TYPES SAME
-	template<typename T, typename...Ts>
-	constexpr inline bool are_same_v = std::conjunction_v<std::is_same<T, Ts>...>;
-
-	// 2. FIRST ARGUMENT IN TYPES LIST
-	template<typename T, typename ...>
-	struct first_arg {
-		using type = T;
-	};
-
-	template<typename...Args>
-	using first_arg_t = typename first_arg<Args...>::type;
-
-
-	// 3. GET A RETURN TYPE OF FUNCTION TEMPLATE OR FUNCTION
-	template<typename T>
-	struct returnType;
-
-	template<typename R, typename ...Ts>
-	struct returnType<R(Ts...)> { using type = R; };
-
-	template<typename T>
-	using returnType_t = typename returnType<T>::type;
-
-	// 4. RETURN LIST OF TYPE OF FUNCTION
-	template<typename...Ts>
-	struct list_types;
-
-	template<typename  Function>
-	struct function_arguments;
-
-	template<typename R, typename ...Args>
-	struct function_arguments<R(Args...)> {
-		using type = list_types<Args...>;
-	};
-
-	template<typename T>
-	using function_arguments_list = typename function_arguments<T>::type;
-
+#define REQUERES(_Class, _Type ,_Function)  requires(_Class C,_Type value){ C._Function(value);}
+#define MAKE_HAS_FUNCTION(_Class, _Type, _Function) template<typename _Class, typename _Type> \
+          concept  Has_##_Function = REQUERES(_Class, _Type, _Function)                       \
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// 
@@ -67,13 +26,13 @@ namespace my_concept {
 
 	// concepte of post increment 
 	template<typename T>
-	concept Pre_Incrementable = requires(T r) { ++r; };
+	concept Pre_Incrementable  = requires(T r) { ++r; };
 
 	template<typename T>
 	concept Post_Incrementable = requires(T r) { r++; };
 
 	template<typename T>
-	concept Pre_Decrementable = requires(T r) { --r; };
+	concept Pre_Decrementable  = requires(T r) { --r; };
 
 	template<typename T>
 	concept Post_Decrementable = requires(T r) { r--; };
@@ -89,16 +48,13 @@ namespace my_concept {
 	concept Addable = requires(Args...args)
 	{
 		(... + args);
-			requires are_same_v<Args...>;
+			requires meta::are_same_v<Args...>;
 			requires sizeof...(Args) > 1;
-		{(... + args)} noexcept -> std::same_as<first_arg_t<Args...>>;
+		{(... + args)} noexcept -> std::same_as<meta::first_arg_t<Args...>>;
 	};
 
 
-#define REQUERES(_Class, _Type ,_Function)  requires(_Class C,_Type value){ C._Function(value);}
 
-#define MAKE_HAS_FUNCTION(_Class, _Type, _Function) template<typename _Class, typename _Type> \
-          concept  Has_##_Function = REQUERES(_Class, _Type, _Function)                       \
 
 
 	template< typename Container, typename T>
