d7d02732f79643b4ae688bd7f022ad2ada8a6722
Kadda Aoues <houes_k@yahoo.fr>
2023-11-18 23:38:07 +0300

    handle window OS specification

---
+++ Console_Library/Event_Windows.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Windows.h>
+
+#define KeyOn(Key)           GetAsyncKeyState(Key) & 0x8000
+#define KeyReleased(Key)     GetAsyncKeyState(Key) & 0x0001
+#define KeyPressed(Key)      GetAsyncKeyState(Key) & 0x8001
+
+#define TITLE(wtitle)         SetConsoleTitle(wtitle)
+
+#define Key(Char)            (unsigned)Char
+
+bool get_out() {
+
+	if ( KeyOn(VK_ESCAPE)) return true;
+	else return false;
+	
+	
+}
