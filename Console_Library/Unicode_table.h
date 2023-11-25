598e67d4b9065e16ee44655edd7a4e896638e34e
Kadda Aoues <houes_k@yahoo.fr>
2023-11-25 17:05:28 +0300

    header comment, add most comment solid wchar

--- Console_Library/Unicode_table.h
+++ Console_Library/Unicode_table.h
@@ -1,3 +1,49 @@
+/*
+ *****************************************************************************
+ *                                                                           *
+ *                          Platform Independent                             *
+ *                      Unicode and Wchar_t Library                          *
+ *                                                                           *
+ * Author: KADDA Aoues - haoues_k@yahoo.fr - 2023                            *
+ *                                                                           *
+ * URL :                                                                     *
+ *                                                                           *
+ * Copyright notice:                                                         *
+ * Free use of the Library CFile                                             *
+ * is permitted under the guidelines and in accordance with the most current *
+ * version of the MIT License.                                               *
+ * http://www.opensource.org/licenses/MIT                                    *
+ *                                                                           *
+ *****************************************************************************
+*/
+
+
+/**********************************************************************************
+ *
+ * Copyright (c) 2022-2023 KADDA Aoues
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ **********************************************************************************/
+
+
+
 #pragma once
 #include <fcntl.h>
 #include <io.h>
@@ -36,5 +82,13 @@ namespace Unicode {
 								 L'\x25a0',  L'\x25aa',  L'\x25ac', // 9,10,11
 								 L'\x25d8',  L'\x25d9',  L'\x25ac'  // 11,12,13
 		};
+
+		const wchar_t solid1 = L'\x2588'  ;
+		const wchar_t solid2 = L'\x25a0'  ;
+		const wchar_t solid3 = L'\x2584'  ;
+		const wchar_t solid4 = L'\x2580'  ;
+		const wchar_t solid5 = L'\x258c'  ;
 	}
 }
+
+namespace wchar = Unicode;
