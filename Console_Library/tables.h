38f909f99dfc64ae570176f80a669abde4f1305e
Kadda Aoues <houes_k@yahoo.fr>
2023-12-08 10:56:36 +0300

    utility ot make table in console

--- Console_Library/tables.h
+++ Console_Library/tables.h
@@ -4,8 +4,8 @@
  *                                                                           *
  *                  Console Table Manipulation  Library                      *
  *                                                                           *
- * Author: KADDA Aoues - haoues_k@yahoo.fr - 2023                            *
- * Date : 27 / 11 / 2023                                                     *
+ * Author : KADDA Aoues - haoues_k@yahoo.fr - 2023                           *
+ * Date   : 27 / 11 / 2023                                                   *
  *                                                                           *
  * Copyright notice:                                                         *
  * Free use of the Library CFile                                             *
@@ -17,46 +17,104 @@
  *
  *
 */
-
+// Todo documenting this header files
 
 #pragma once
+
+#include <iomanip>
+#include "MyLib/Console_Library/escape_code.h"
+
 
 #define bg_color(i)      "\x1b[48;5;"#i"m" 
 #define fg_color(i)      "\x1b[38;5;"#i"m"
 
-struct TITLE {
-    std::string _name;
-    std::string _fgcolor;
-    std::string _bgcolor;
-    int         _width;
-    char        _sep0;
-    char        _sep1;
-};
+// here will define table box for making table in char;
+// 1. table box
 
-// Contructor for TITLE stuct
-#define CellCtor(T,name, bg, fg, width, sep0 , sep1 )     	TITLE T;     \
-   /* define background color */     T._bgcolor = bg_color(bg);			 \
-   /* define text color */           T._fgcolor = fg_color(fg);			 \
-   /* define name of cell */         T._name = #name;					 \
-   /* define width of cell */        T._width = width;					 \
-   /* define first separator '|' */  T._sep0 = sep0;					 \
-   /* define last separator '|' */   T._sep1 = sep1;					 \
-
-// macro table formatting
-#define _cell(t,right_left)             t._sep0                \
-                                     << t._fgcolor             \
-                                     << t._bgcolor             \
-                                     << std::setw(t._width)    \
-                                     << std::right_left        \
-                                     << t._name << RESETMODE   \
-                                     << t._sep1                \
-
-// Macro : Draw from struct title 
-#define _cellv(t,right_left, value)   t._sep0 << t._fgcolor << t._bgcolor << std::setw(t._width)   \
-                                       << std::right_left << value << RESETMODE << t._sep1 \
-
-#define _cellr(sleft, fg, bg, width, right_left , value , srigth)       sleft                 \
-                            << fg << bg << std::setw(width)          \
-                            << std::right_left << value << RESETMODE << srigth  \
+#define table_vline                         '\xb3'
+#define table_hline                         '\xc4'
+#define table_tee_left                      '\xb4'
+#define table_tee_right                     '\xc3'
+#define table_tee_up                        '\xc1'
+#define table_tee_down                      '\xc2'
+#define table_xross                         '\xc5'
+#define table_corner_upleft                 '\xda'
+#define table_corner_upright                '\xbf'
+#define table_corner_dnleft                 '\xc0'
+#define table_corner_dnright                '\xd9'
 
 
+// 2. solid 
+#define  table_solid_left         '\xdd'
+#define  table_solid_right        '\xde'
+#define  table_solid_complete     '\xdb'
+#define  table_solid_up           '\xdf'
+#define  table_solid_down         '\xdc'
+#define  table_solid_center       '\xfe'
+
+
+namespace table {
+    // making global variable 
+    int _xPos{};
+    int _yPos{};
+
+    // function initialize these varible
+    void init_coordinate_table(const int x, const int y) {
+        _xPos = x;
+        _yPos = y;
+    }
+
+    // style for cells 
+    struct STYLE {
+        int   _fg_color;     // foreground color , text color
+        int   _bg_color;     // background color , back area color
+        int   _width;        // width of cell , 
+        char  _b_sep;   // b for beginerdelemeter of cell like bar '|' or another glyph bars
+        char  _e_sep;   // end delemeter if not equal to beginer, or is null,
+    };
+
+    // function to constructe styles in place
+    STYLE make_style(int fg_col, int bg_col, int width, char begin, char end) {
+        return STYLE{
+            ._fg_color = fg_col,
+            ._bg_color = bg_col,
+            ._width    = width,
+            ._b_sep    = begin,
+            ._e_sep    = end,
+       };
+    }
+
+    // make macro that affect any text to cell style: we keep separator uncolored
+    #define    _cell(text, style, left_right)       style._b_sep << _COLOR_BG256( style._bg_color)    \
+                                                   << _COLOR_FG256(style._fg_color)    \
+                                                   << std::setw(style._width)    \
+                                                   << std::left_right            \
+                                                   << text << RESETMODE          \
+                                                   << style._e_sep          \
+    
+
+    void make_simple_table( const std::string& title,        STYLE s_title,
+                            const std::string& sub_title_l,  STYLE s_sub_left,
+                            const std::string& sub_title_r,  STYLE s_sub_right  );
+
+
+    template<typename T>
+    std::string add_line_table(const T& cell_l, STYLE sleft, const T& cell_r, STYLE sright) {
+        std::stringstream ss;
+
+        ss << UNDERLINE << _cell(std::to_string(cell_l), sleft, right)
+           << UNDERLINE << _cell(std::to_string(cell_r), sright, right) << end_;
+
+        return ss.str();
+    }
+
+
+    class ITable {
+    public:
+
+
+
+        virtual ~ITable() {}
+    };
+
+}
