b054bda8efa3a2bdd1c2a37d17ba51fd33c92d89
Kadda Aoues <houes_k@yahoo.fr>
2024-02-09 10:03:53 +0300

    new light timer

---
+++ chrono/Game_timer.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <chrono>
+
+
+
+class Game_Timer {
+	std::chrono::steady_clock::time_point _start;
+	std::chrono::steady_clock::time_point _start0;
+
+public:
+	Game_Timer()
+		:_start{std::chrono::steady_clock::now()}
+		, _start0(_start)
+	{
+		
+	}
+
+	float get_elapsed_time() {
+		return std::chrono::duration<float>(std::chrono::steady_clock::now() - _start).count();
+	}
+
+	void reset() {
+		_start = std::chrono::steady_clock::now();
+	}
+
+	float time_since_begin() {
+		return std::chrono::duration<float>(std::chrono::steady_clock::now() - _start0).count();
+	}
+};
