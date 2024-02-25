0fffe3254e09c50cdac21d2006aa134ba82d5041
Kadda Aoues <houes_k@yahoo.fr>
2024-02-25 04:20:24 +0300

    game timer, simple class

--- chrono/Game_timer.h
+++ chrono/Game_timer.h
@@ -1,29 +1,39 @@
 #pragma once
 #include <chrono>
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+//    Simple class that hold elapsed time.
+// 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-class Game_Timer {
-	std::chrono::steady_clock::time_point _start;
-	std::chrono::steady_clock::time_point _start0;
 
-public:
-	Game_Timer()
-		:_start{std::chrono::steady_clock::now()}
-		, _start0(_start)
-	{
-		
-	}
+namespace Time {
 
-	float get_elapsed_time() {
-		return std::chrono::duration<float>(std::chrono::steady_clock::now() - _start).count();
-	}
+	class Game_Timer {
+		std::chrono::steady_clock::time_point _start;
+		std::chrono::steady_clock::time_point _start0;
 
-	void reset() {
-		_start = std::chrono::steady_clock::now();
-	}
+	public:
+		Game_Timer()
+			:_start{ std::chrono::steady_clock::now() }
+			, _start0(_start)
+		{
 
-	float time_since_begin() {
-		return std::chrono::duration<float>(std::chrono::steady_clock::now() - _start0).count();
-	}
-};
+		}
+
+		float get_elapsed_time() {
+			return std::chrono::duration<float>(std::chrono::steady_clock::now() - _start).count();
+		}
+
+		void reset() {
+			_start = std::chrono::steady_clock::now();
+		}
+
+		float time_since_begin() {
+			return std::chrono::duration<float>(std::chrono::steady_clock::now() - _start0).count();
+		}
+	};
+
+}
