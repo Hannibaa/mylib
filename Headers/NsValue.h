a6760ff7f470e56fe910930d4fbb7c043261e49c
Kadda Aoues <houes_k@yahoo.fr>
2024-04-02 23:03:13 +0300

    Need revision of this class

--- Headers/NsValue.h
+++ Headers/NsValue.h
@@ -6,13 +6,15 @@
           THIS CLASS OF ARRAY OF NUMBER WILL STORE ARRAY OF SIZE N,
 		  AND EQUIPED BY CONVERSION OPERATOR THAT RETURN A RANDOM VALUE FROM THIS ARRAY
 
+		  TODO : 
+		  1. constructors and more vectors and array extend to any class type .
 */
 
 
 template<typename T, size_t N>
 class NsValue {
 	static_assert(std::is_trivially_copyable_v<T>, "T type should be copyable");
-	T x[N];
+	T x[N]{};
 
 public:
 
@@ -24,7 +26,7 @@ public:
 
 	template<typename ...Ts>
 	NsValue(Ts...ts) : x{ T(ts)... } {
-		static_assert(N == sizeof...(Ts), "number of argument should be equal to size of N");
+		static_assert(N <= sizeof...(Ts), "number of argument should be equal to size of N");
 		//T x1 = {T(ts)...};
 		puts("NsValue(Ts...ts)");
 	}
@@ -42,7 +44,7 @@ template<typename T, size_t N>
 NsValue(const T(&)[N]) -> NsValue<T, N>;
 
 template<typename...Ts>
-NsValue(Ts...) -> NsValue<typename my_mp::first_type<Ts...>::type, sizeof...(Ts)>;
+NsValue(Ts...) -> NsValue<typename meta::first_type<Ts...>::type, sizeof...(Ts)>;
 
 template<typename T, size_t N>
 void print_value(NsValue<T, N>& ns_value) {
